c9e2364e5a83b45b45ebcd952c5d7364a90e3e8d
Malwrologist <9646319+DissectMalware@users.noreply.github.com>
2022-01-30 05:41:05 -0800

    ADD - favicon

--- webpack.core.js
+++ webpack.core.js
@@ -27,6 +27,7 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 { from: "src/img", to: "img" },
+                { from: "src/favicon.ico", to: "./" }
             ]
         })
     ],
